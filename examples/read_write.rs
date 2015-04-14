4a18832e95f5f56bce9fc761aa7866cc6db988dc
David Cuddeback <david.cuddeback@gmail.com>
2015-04-13 20:56:20 -0700

    adds example of reading and writing to a port

---
+++ examples/read_write.rs
@@ -0,0 +1,41 @@
+extern crate serial;
+extern crate time;
+
+use std::env;
+use std::io;
+
+use time::Duration;
+
+use std::io::prelude::*;
+use serial::prelude::*;
+
+fn main() {
+    for arg in env::args_os().skip(1) {
+        println!("opening port: {:?}", arg);
+        let mut port = serial::open(&arg).unwrap();
+
+        interact(&mut port).unwrap();
+    }
+}
+
+fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
+    try!(port.configure(|settings| {
+        settings.set_baud_rate(serial::Baud9600);
+        settings.set_char_size(serial::Bits8);
+        settings.set_parity(serial::ParityNone);
+        settings.set_stop_bits(serial::Stop1);
+        settings.set_flow_control(serial::FlowNone);
+    }));
+
+    port.set_timeout(Duration::milliseconds(1000));
+
+    let mut buf: Vec<u8> = (0..255).collect();
+
+    println!("writing bytes");
+    try!(port.write(&buf[..]));
+
+    println!("reading bytes");
+    try!(port.read(&mut buf[..]));
+
+    Ok(())
+}
