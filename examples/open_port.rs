78be09631dd571cfcc6d360a430c6d0d5781f87f
David Cuddeback <david.cuddeback@gmail.com>
2015-04-17 08:32:08 -0700

    adds examples of opening a serial port

---
+++ examples/open_port.rs
@@ -0,0 +1,16 @@
+extern crate serial;
+
+#[cfg(unix)]
+fn main() {
+    use std::path::Path;
+
+    serial::open("/dev/ttyUSB0").unwrap();
+    serial::open(Path::new("/dev/ttyUSB0")).unwrap();
+    serial::posix::TTYPort::open(Path::new("/dev/ttyUSB0")).unwrap();
+}
+
+#[cfg(windows)]
+fn main() {
+    serial::open("COM1").unwrap();
+    serial::windows::COMPort::open("COM1").unwrap();
+}
