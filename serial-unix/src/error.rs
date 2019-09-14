d6acc67a2482dc6927e3af92376bdf4b624e6da0
Sylwester Rąpała <sylwester.rapala@igrid.technology>
2019-09-14 12:22:43 +0200

    Apply rustfmt

--- serial-unix/src/error.rs
+++ serial-unix/src/error.rs
@@ -5,22 +5,27 @@ use std::ffi::CStr;
 use std::io;
 use std::str;
 
-use libc::{c_int, c_char, size_t};
+use libc::{c_char, c_int, size_t};
 
 pub fn last_os_error() -> core::Error {
     from_raw_os_error(errno())
 }
 
 pub fn from_raw_os_error(errno: i32) -> core::Error {
-    use libc::{EBUSY, EISDIR, ELOOP, ENOTDIR, ENOENT, ENODEV, ENXIO, EACCES, EINVAL, ENAMETOOLONG, EINTR, EWOULDBLOCK};
+    use libc::{
+        EACCES, EBUSY, EINTR, EINVAL, EISDIR, ELOOP, ENAMETOOLONG, ENODEV, ENOENT, ENOTDIR, ENXIO,
+        EWOULDBLOCK,
+    };
 
     let kind = match errno {
-        EBUSY | EISDIR | ELOOP | ENOTDIR | ENOENT | ENODEV | ENXIO | EACCES => core::ErrorKind::NoDevice,
+        EBUSY | EISDIR | ELOOP | ENOTDIR | ENOENT | ENODEV | ENXIO | EACCES => {
+            core::ErrorKind::NoDevice
+        }
         EINVAL | ENAMETOOLONG => core::ErrorKind::InvalidInput,
 
-        EINTR       => core::ErrorKind::Io(io::ErrorKind::Interrupted),
+        EINTR => core::ErrorKind::Io(io::ErrorKind::Interrupted),
         EWOULDBLOCK => core::ErrorKind::Io(io::ErrorKind::WouldBlock),
-        _           => core::ErrorKind::Io(io::ErrorKind::Other),
+        _ => core::ErrorKind::Io(io::ErrorKind::Other),
     };
 
     core::Error::new(kind, error_string(errno))
@@ -44,51 +49,55 @@ const TMPBUF_SZ: usize = 128;
 pub fn errno() -> i32 {
     #[cfg(any(target_os = "macos", target_os = "ios", target_os = "freebsd"))]
     unsafe fn errno_location() -> *const c_int {
-        extern { fn __error() -> *const c_int; }
+        extern "C" {
+            fn __error() -> *const c_int;
+        }
         __error()
     }
 
     #[cfg(target_os = "bitrig")]
     fn errno_location() -> *const c_int {
-        extern {
+        extern "C" {
             fn __errno() -> *const c_int;
         }
-        unsafe {
-            __errno()
-        }
+        unsafe { __errno() }
     }
 
     #[cfg(target_os = "dragonfly")]
     unsafe fn errno_location() -> *const c_int {
-        extern { fn __dfly_error() -> *const c_int; }
+        extern "C" {
+            fn __dfly_error() -> *const c_int;
+        }
         __dfly_error()
     }
 
     #[cfg(target_os = "openbsd")]
     unsafe fn errno_location() -> *const c_int {
-        extern { fn __errno() -> *const c_int; }
+        extern "C" {
+            fn __errno() -> *const c_int;
+        }
         __errno()
     }
 
     #[cfg(any(target_os = "linux", target_os = "android"))]
     unsafe fn errno_location() -> *const c_int {
-        extern { fn __errno_location() -> *const c_int; }
+        extern "C" {
+            fn __errno_location() -> *const c_int;
+        }
         __errno_location()
     }
 
-    unsafe {
-        (*errno_location()) as i32
-    }
+    unsafe { (*errno_location()) as i32 }
 }
 
 pub fn error_string(errno: i32) -> String {
     #[cfg(target_os = "linux")]
-    extern {
+    extern "C" {
         #[link_name = "__xpg_strerror_r"]
         fn strerror_r(errnum: c_int, buf: *mut c_char, buflen: size_t) -> c_int;
     }
     #[cfg(not(target_os = "linux"))]
-    extern {
+    extern "C" {
         fn strerror_r(errnum: c_int, buf: *mut c_char, buflen: size_t) -> c_int;
     }
 
@@ -101,6 +110,8 @@ pub fn error_string(errno: i32) -> String {
         }
 
         let p = p as *const _;
-        str::from_utf8(CStr::from_ptr(p).to_bytes()).unwrap().to_string()
+        str::from_utf8(CStr::from_ptr(p).to_bytes())
+            .unwrap()
+            .to_string()
     }
 }
--- serial-unix/src/lib.rs
+++ serial-unix/src/lib.rs
@@ -1,9 +1,9 @@
 //! Serial port implementation for Unix operating systems.
 
-extern crate serial_core as core;
-extern crate libc;
-extern crate termios;
 extern crate ioctl_rs as ioctl;
+extern crate libc;
+extern crate serial_core as core;
+extern crate termios;
 
 pub use tty::*;
 
